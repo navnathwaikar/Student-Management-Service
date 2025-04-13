54b273de0b66042786ee70376305493e5dbd94a7
Navnath Waikar <nwaikar19@gmail.com>
2025-04-14 01:14:32 +0530

    Swagger added

--- src/main/java/com/example/controller/StudentReceiptController.java
+++ src/main/java/com/example/controller/StudentReceiptController.java
@@ -3,6 +3,9 @@ package com.example.controller;
 import com.example.dto.ReceiptDTO;
 import com.example.dto.ReceiptGetDetailsDTO;
 import com.example.service.StudentReceiptService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,6 +24,11 @@ public class StudentReceiptController {
     StudentReceiptService studentReceiptService;
 
 
+    @Operation(summary = "Get student Receipt by ID", description = "Returns a Fee Receipt w.r.t student if found")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the student receipt details"),
+            @ApiResponse(responseCode = "404", description = "Student Receipt not found")
+    })
     @GetMapping("/{studentId}")
     public ResponseEntity<ReceiptGetDetailsDTO> getReceiptByStudentId(@PathVariable Long studentId){
         logger.info("Student Receipt Details start with id={}", studentId);
